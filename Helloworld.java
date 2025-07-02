93eed33477bc4e42d3fadec5a2c2927efb8d3caf
DEVINMAKING14 <namantiwari1414@gmail.com>
2025-07-02 21:16:18 +0530

    Create Helloworld.java
    First program on github

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+class Helloworld                      //iNITIALISING CLASS NAME
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Helloworld");                 //PRINTING STATEMENT
+    }
+}                   //ENDING OF THE CLASS 
