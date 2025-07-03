3efb20c771114bb8699466e5a1c3263a462d18cf
DEVINMAKING14 <namantiwari1414@gmail.com>
2025-07-03 23:25:40 +0530

    Create multiplication.java
    A simple program for multiplication by taking input from the user.

---
+++ multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class multiplication 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("ENTER THE FIRST NUMBER");
+        int n= sc.nextInt();
+        System.out.println("ENTER THE SECOND NUMBER");
+        int j= sc.nextInt();
+        int M= n*j;
+        System.out.println("THE OUTPUT IS : "+M);
+    }
+}
