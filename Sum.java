657bf53bdd1e6ae60a555b99f14378ea7cad963d
DEVINMAKING14 <namantiwari1414@gmail.com>
2025-07-03 23:15:28 +0530

    Create Sum.java
    Wrote a clean Java program to add two numbers...

---
+++ Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Sum
+{
+    public static void main(String[] args) 
+    {
+        int k =0;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("ENTER THE FIRST NUMBER");
+        int n=sc.nextInt();
+        System.out.println("ENTER THE SECOND NUMBER");
+        int j=sc.nextInt();
+        k = n+j;
+        System.out.println("THE SUM OF THE TWO NUMBERS ARE : "+ k);
+    }
+}
