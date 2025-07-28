834edf3db0c64ddb7b24ad2179e315b8bd5ef78c
DEVINMAKING14 <namantiwari1414@gmail.com>
2025-07-28 23:13:35 +0530

    Allcounter
    The program counts all the Alphabets, Digits and the Special Character

---
+++ allcounters.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class allcounters{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Counters
+        int vowels = 0, consonants = 0, digits = 0, specialChars = 0;
+
+        // Convert input to lowercase for easy comparison
+        input = input.toLowerCase();
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if ("aeiou".indexOf(ch) != -1) {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            } else if (ch >= '0' && ch <= '9') {
+                digits++;
+            } else if (ch != ' ') {
+                specialChars++;
+            }
+        }
+
+        // Output
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("Special Characters: " + specialChars);
+
+        sc.close();
+    }
+}
